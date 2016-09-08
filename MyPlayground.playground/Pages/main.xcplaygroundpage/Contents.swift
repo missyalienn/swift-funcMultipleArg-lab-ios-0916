ffcca87096ba18c5a27325ebacc01e1ede5d7704
Missy Allan <missyallan@Melissas-MacBook-Air.local>
2016-09-08 15:26:35 -0400

    functions w multiple arguments done.

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,19 +13,20 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
 
-
-
+func countriesOfContinent(nameOfContinent: String, numberOfCountries: Int) {
+    print("\(nameOfContinent) is a continent which contains \(numberOfCountries) countries.")
+}
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
-func greeting(name: String, greeting: String) {
+
+func sayHelloToPerson(name: String, greeting: String){
     print("\(greeting), \(name)!")
 }
 
-greeting("Danny", "Hello")
+sayHelloToPerson("Danny", greeting: "Hello")
 
 
 
@@ -33,19 +34,22 @@ greeting("Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
 daysInMonth("November", days: 30)
 
 
-
-
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func bottlesOfBeverage(numberOfBottles:String, nameOfBeverage:String, beverageStorageLocation: String) {
+ print ("\(numberOfBottles) bottles of \(nameOfBeverage) on the \(beverageStorageLocation) wall.")
+}
+
+bottlesOfBeverage("Ninety-Nine", nameOfBeverage: "champagne", beverageStorageLocation: "refrigorator")
+
 
 
 
